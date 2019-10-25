7e36457b3454b4a6e583b4870cd39b4113cc3a80
Anirudhsfc <anirudhsfc@gmail.com>
2019-10-26 05:14:27 +0800

    backend pbi

--- productBacklog/models.py
+++ productBacklog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from products.models import User
 # Create your models here.
+
+class ProductBacklog(models.Model):
+    title=models.CharField(max_length=100)
+    effort=models.DecimalField(max_digits=100,decimal_places=3)
+    story_points=models.DecimalField(max_digits=100,decimal_places=3)
+    effort_done=models.DecimalField(max_digits=100,decimal_places=3)
+    status=models.CharField(max_length=2,choices=[('TD','To-Do'),('P','In Progress'),('D','Done')],default='TD')
+    last_updated=models.DateTimeField(auto_now=False,auto_now_add=False)
+    pbi_type=models.CharField(max_length=2,choices=[('B','Bug'),('E','Epic'),('US','User Story')],default='US')
+    sprint_no=models.PositiveSmallIntegerField()
+    main_dev=models.ForeignKey(User,on_delete=models.CASCADE,related_name="main_project_pbis")
+    other_dev=models.ManyToManyField(User)
+    priority=models.DecimalField(max_digits=100,decimal_places=3)
+
