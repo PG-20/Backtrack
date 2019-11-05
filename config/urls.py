4e09ba6f95aa507367b24664c74979934269c736
Anirudhsfc <anirudhsfc@gmail.com>
2019-11-06 03:01:12 +0800

    Sprint

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from productBacklog.views import index
-from task.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
